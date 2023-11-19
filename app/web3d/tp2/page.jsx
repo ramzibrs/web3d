032511ab7f1148bc7f7f3d27a7c8adae173933bc
ramzi <ramzi.bouras0@gmail.com>
2023-11-19 21:43:04 +0100

    second commit

--- app/web3d/tp2/page.jsx
+++ app/web3d/tp2/page.jsx
@@ -1,8 +1,17 @@
+"use client";
+
 import React from 'react'
+import scene1 from '@/public/pictures/scene.gif'
+import Image from 'next/image'
+import Link from 'next/link'
+import '@/css/style/tp2/style.css'
 
 function scene() {
   return (
-    <div>tp2 yji hna</div>
+    <div className="container">
+    <h1>Ma Page Web avec un GIF</h1>
+    <Image src={scene1} alt="Votre Photo" />
+  </div>
   )
 }
 
