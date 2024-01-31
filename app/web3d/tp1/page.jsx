2c58a56a562884358cf6653aa0b5a6d77aadd876
ramzi <ramzi.bouras0@gmail.com>
2024-01-31 01:00:17 +0100

    p1

--- app/web3d/tp1/page.jsx
+++ app/web3d/tp1/page.jsx
@@ -1,34 +1,30 @@
-
 "use client";
 
 import React from "react";
 import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
-import {Model} from '@/components/Table'
+import { Model } from "@/components/Table";
 
 function Tp1() {
   return (
-
     <div>
-        <Canvas
-          camera={{ position: [-10, 5, 7] }}
-          style={{ width: "1280px", height: "720px" }}
-        >
-          
-          <ambientLight intensity={0.5} />
-          
-          <directionalLight position={[20, 10, 10]} intensity={1} />
-          <directionalLight position={[0, 0, 0]} intensity={1} />
-          <directionalLight position={[-10, -10, -10]} intensity={1} />
-          <directionalLight position={[-20, -20, -20]} intensity={1} />
-          
-          <pointLight position={[10, 10, 10]} />
-          <Model /> 
-          <OrbitControls />
-        </Canvas>
+      <Canvas
+        camera={{ position: [-10, 5, 40], fov: 60 }}
+        style={{ width: "1280px", height: "720px" }}
+      >
+        <ambientLight intensity={0.5} />
+
+        <directionalLight position={[20, 10, 10]} intensity={1} />
+        <directionalLight position={[0, 0, 0]} intensity={1} />
+        <directionalLight position={[-10, -10, -10]} intensity={1} />
+        <directionalLight position={[-20, -20, -20]} intensity={1} />
+
+        <pointLight position={[10, 10, 10]} />
+        <Model />
+        <OrbitControls />
+      </Canvas>
     </div>
-    
-  )
+  );
 }
 
-export default Tp1
+export default Tp1;
